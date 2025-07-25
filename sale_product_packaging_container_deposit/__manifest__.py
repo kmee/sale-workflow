78002415d5dc2ccb62410582f29deb4352359893
Daniel Sadamo <daniel.sadamo@kmee.com.br>
2025-07-25 18:38:29 -0300

    [MIG] sale_product_packaging_container_deposit: migrate to v17.0.1.0.0 with installable=False

--- sale_product_packaging_container_deposit/__manifest__.py
+++ sale_product_packaging_container_deposit/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Sale Product Packaging Container Deposit",
-    "version": "16.0.1.1.1",
+    "version": "17.0.1.0.0",
     "development_status": "Beta",
     "category": "Product",
     "summary": "Sale Product Packaging Container Deposit",

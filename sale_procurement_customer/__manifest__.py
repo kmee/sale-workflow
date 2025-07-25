bdd188a48cccbbb424ca6ab761d5f33b6e1a33ec
Daniel Sadamo <daniel.sadamo@kmee.com.br>
2025-07-25 18:38:28 -0300

    [MIG] sale_procurement_customer: migrate to v17.0.1.0.0 with installable=False

--- sale_procurement_customer/__manifest__.py
+++ sale_procurement_customer/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Sale Procurement Customer",
     "summary": """
         Allows to transmit the customer to the procurement group""",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/sale-workflow",
@@ -13,4 +13,5 @@
         "sale_stock",
         "stock_procurement_customer",
     ],
+    "installable": False,
 }

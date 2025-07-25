bf401bc6057eb8caf6edf144e773a7f4cd9f3938
Daniel Sadamo <daniel.sadamo@kmee.com.br>
2025-07-25 18:38:29 -0300

    [MIG] sale_stock_expiry_date_on_qty_at_date_widget: migrate to v17.0.1.0.0 with installable=False

--- sale_stock_expiry_date_on_qty_at_date_widget/__manifest__.py
+++ sale_stock_expiry_date_on_qty_at_date_widget/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Sale Stock Expiry Date on Qty at Date widget",
     "summary": "Show next Expiry Date on Qty at Date Widget",
-    "version": "16.0.1.0.1",
+    "version": "17.0.1.0.0",
     "development_status": "Alpha",
     "category": "Sales",
     "website": "https://github.com/OCA/sale-workflow",

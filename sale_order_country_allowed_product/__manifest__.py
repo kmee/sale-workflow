f5300aa46916ff088150f53cc0e0bfa8f8c952f9
Daniel Sadamo <daniel.sadamo@kmee.com.br>
2025-07-25 16:08:16 -0300

    [MIG] sale_order_country_allowed_product: set installable=False

--- sale_order_country_allowed_product/__manifest__.py
+++ sale_order_country_allowed_product/__manifest__.py
@@ -10,7 +10,7 @@
     "author": "Sygel, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
-    "installable": True,
+    "installable": False,
     "depends": [
         "sale",
     ],

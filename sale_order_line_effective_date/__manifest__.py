d667e03772042120efbf8d5f1e8e7fcedb72fa28
Daniel Sadamo <daniel.sadamo@kmee.com.br>
2025-07-25 18:38:29 -0300

    [MIG] sale_order_line_effective_date: migrate to v17.0.1.0.0 with installable=False

--- sale_order_line_effective_date/__manifest__.py
+++ sale_order_line_effective_date/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Sale Order Line Effective Dates",
     "summary": "Calculated effective dates in Sale Order Lines",
-    "version": "16.0.1.1.0",
+    "version": "17.0.1.0.0",
     "development_status": "Alpha",
     "category": "Sales/Sales",
     "website": "https://github.com/OCA/sale-workflow",

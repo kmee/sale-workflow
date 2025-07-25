6fb6be8d2accec247b34ae6c54cc977d66068bf9
Daniel Sadamo <daniel.sadamo@kmee.com.br>
2025-07-25 18:38:29 -0300

    [MIG] sale_stock_reservation_issue_on_qty_at_date_widget: migrate to v17.0.1.0.0 with installable=False

--- sale_stock_reservation_issue_on_qty_at_date_widget/__manifest__.py
+++ sale_stock_reservation_issue_on_qty_at_date_widget/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Sale Stock Reservation Issue on Qty at Date widget",
     "summary": "Warn user when a reservation issue will happen when confirming an order",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "development_status": "Alpha",
     "category": "Sales",
     "website": "https://github.com/OCA/sale-workflow",

f660af8558b496ffa0b33c487bd498d64f400fb6
Daniel Sadamo <daniel.sadamo@kmee.com.br>
2025-07-25 18:38:28 -0300

    [MIG] sale_pricelist_triple_discount: migrate to v17.0.1.0.0 with installable=False

--- sale_pricelist_triple_discount/__manifest__.py
+++ sale_pricelist_triple_discount/__manifest__.py
@@ -4,7 +4,7 @@
 #  License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Sale Pricelist Triple Discount",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "category": "Sale",
     "author": "Agile Business Group, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/sale-workflow"

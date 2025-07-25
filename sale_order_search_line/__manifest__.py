1746e88df8188d9b391a42de72806ac49ddae9f6
Daniel Sadamo <daniel.sadamo@kmee.com.br>
2025-07-25 18:38:28 -0300

    [MIG] sale_order_search_line: migrate to v17.0.1.0.0 with installable=False

--- sale_order_search_line/__manifest__.py
+++ sale_order_search_line/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Sale Order Search Line",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "author": "Quartile, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/sale-workflow",
     "category": "Sales",
